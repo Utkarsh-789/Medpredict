5cddc56a2dcf791bd44d2207d4283825d4d3b737
Utkarsh Verma <155562245+Utkarsh-789@users.noreply.github.com>
2026-08-24 16:06:24 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
+import os
 import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Victus@456",   # 🔴 change this
-        database="medpredict"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=False
     )
